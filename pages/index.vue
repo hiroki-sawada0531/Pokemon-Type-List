ad783f696b4a123220320c756aaa06f6320b53be
澤田大樹 <hiroki.bigtree@icloud.com>
2020-11-13 23:51:33 +0900

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,37 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        pokemon
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div v-for="(pokemon, index) in pokemons" :key="index" class="constent">
+      <div>{{ pokemon.name }}　{{ pokemon.types[0] }}, {{ pokemon.types[1] }}</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
-</script>
+const axios = require('axios')
+const url = 'https://raw.githubusercontent.com/kotofurumiya/pokemon_data/master/data/pokemon_data.json'
 
+export default {
+  asyncData({ params, error }) {
+    return axios
+    .get(url)
+    .then((res) => {
+      console.log(res.data)
+      return { pokemons: res.data }
+    })
+    .catch((e) => {
+      error({ pokemon: e.response.status, message: 'ERROR!!' })
+    })
+  }
+}
+</script>
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
+  margin: 50px auto;
+  padding: 0;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.constent {
+  margin: 0 auto;
 }
 </style>
